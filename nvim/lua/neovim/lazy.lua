628176f55ba48f6549208e907d082875d3c612c1
icoderarely <166960965+icoderarely@users.noreply.github.com>
2024-05-05 02:36:22 +0530

    bufferline

--- nvim/lua/neovim/lazy.lua
+++ nvim/lua/neovim/lazy.lua
@@ -14,7 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{ import = "neovim.plugins" },
 	{ import = "neovim.plugins.lsp" },
-	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
 }, {
 	checker = {
 		enabled = true,
